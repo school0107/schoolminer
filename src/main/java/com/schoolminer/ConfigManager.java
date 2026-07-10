f43eba828e3d927f7735f4e65da31b7e42c11e84
school0107 <school010710@gmail.com>
2026-07-10 16:17:45 +0700

    Update ConfigManager.java

--- src/main/java/com/schoolminer/ConfigManager.java
+++ src/main/java/com/schoolminer/ConfigManager.java
@@ -11,6 +11,16 @@ public class ConfigManager {
     private int mineDelay;
     private int attackDelay;
     private int maxRange;
+    private int killRange;
+    private double baseDamage;
+    private boolean killMonster;
+    private boolean killAnimal;
+    private boolean killMob;
+    private boolean dropItems;
+    private boolean dropXp;
+    private int xpMonster;
+    private int xpAnimal;
+    private int xpMob;
 
     public ConfigManager(Schoolminer plugin) {
         this.plugin = plugin;
@@ -21,6 +31,7 @@ public class ConfigManager {
         plugin.reloadConfig();
         FileConfiguration config = plugin.getConfig();
         
+        // Auto Mine
         whitelist = new HashSet<>();
         List<String> blockNames = config.getStringList("whitelist");
         for (String name : blockNames) {
@@ -32,9 +43,21 @@ public class ConfigManager {
             }
         }
         
-        mineDelay = config.getInt("settings.mine-delay", 5);
-        attackDelay = config.getInt("settings.attack-delay", 10);
-        maxRange = config.getInt("settings.max-range", 5);
+        mineDelay = config.getInt("automine.delay", 20);
+        maxRange = config.getInt("automine.max-range", 5);
+        
+        // Auto Kill
+        attackDelay = config.getInt("autokill.attack-delay", 40);
+        killRange = config.getInt("autokill.range", 3);
+        baseDamage = config.getDouble("autokill.base-damage", 4.0);
+        killMonster = config.getBoolean("autokill.kill-monster", true);
+        killAnimal = config.getBoolean("autokill.kill-animal", false);
+        killMob = config.getBoolean("autokill.kill-mob", true);
+        dropItems = config.getBoolean("autokill.drop-items", true);
+        dropXp = config.getBoolean("autokill.drop-xp", true);
+        xpMonster = config.getInt("autokill.xp-monster", 5);
+        xpAnimal = config.getInt("autokill.xp-animal", 1);
+        xpMob = config.getInt("autokill.xp-mob", 3);
     }
 
     public boolean isWhitelisted(Material material) {
@@ -51,6 +74,46 @@ public class ConfigManager {
 
     public int getMaxRange() {
         return maxRange;
+    }
+
+    public int getKillRange() {
+        return killRange;
+    }
+
+    public double getBaseDamage() {
+        return baseDamage;
+    }
+
+    public boolean isKillMonster() {
+        return killMonster;
+    }
+
+    public boolean isKillAnimal() {
+        return killAnimal;
+    }
+
+    public boolean isKillMob() {
+        return killMob;
+    }
+
+    public boolean isDropItems() {
+        return dropItems;
+    }
+
+    public boolean isDropXp() {
+        return dropXp;
+    }
+
+    public int getXpMonster() {
+        return xpMonster;
+    }
+
+    public int getXpAnimal() {
+        return xpAnimal;
+    }
+
+    public int getXpMob() {
+        return xpMob;
     }
 
     public String getMessage(String key) {

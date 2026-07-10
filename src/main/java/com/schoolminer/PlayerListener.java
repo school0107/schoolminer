481873e3f00b5324f988e9d22c6b46b3824a63e1
school0107 <school010710@gmail.com>
2026-07-10 14:16:44 +0700

    Create PlayerListener.java

---
+++ src/main/java/com/schoolminer/PlayerListener.java
@@ -0,0 +1,29 @@
+package com.schoolminer;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+
+public class PlayerListener implements Listener {
+    private final Schoolminer plugin;
+
+    public PlayerListener(Schoolminer plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        plugin.getAutoMineManager().stopMining(player);
+        plugin.getAutoKillManager().stopKilling(player);
+    }
+
+    @EventHandler
+    public void onPlayerKick(PlayerKickEvent event) {
+        Player player = event.getPlayer();
+        plugin.getAutoMineManager().stopMining(player);
+        plugin.getAutoKillManager().stopKilling(player);
+    }
+}
